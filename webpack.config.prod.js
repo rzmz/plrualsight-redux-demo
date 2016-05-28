9bffadf0f9eac44a5f8b7c50814fec740083b395
Rasmus Piho <rasmus.piho@gmail.com>
2016-05-28 12:39:46 +0300

    Final commit after completing the Production Builds module

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,16 @@
 import webpack from 'webpack';
 import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const GLOBALS = {
+	'process.env.NODE_ENV': JSON.stringify('production')
+};
 
 export default {
 	debug: true,
-	devtool: 'cheap-module-eval-source-map',
+	devtool: 'source-map',
 	noInfo: false,
-	entry: [
-		'eventsource-polyfill', // necessary for hot reloading with IE
-		'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-		'./src/index'
-	],
+	entry: './src/index',
 	target: 'web',
 	output: {
 		path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
@@ -17,20 +18,23 @@ export default {
 		filename: 'bundle.js'
 	},
 	devServer: {
-		contentBase: './src'
+		contentBase: './dist'
 	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.DefinePlugin(GLOBALS),
+		new ExtractTextPlugin('styles.css'),
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin()
 	],
 	module: {
 		loaders: [{
 			test: /\.js$/,
 			include: path.join(__dirname, 'src'),
-			loaders: ['babel']
+			loader: 'babel'
 		}, {
 			test: /(\.css)$/,
-			loaders: ['style', 'css']
+			loader: ExtractTextPlugin.extract('css?sourceMap')
 		}, {
 			test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
 			loader: 'file'
